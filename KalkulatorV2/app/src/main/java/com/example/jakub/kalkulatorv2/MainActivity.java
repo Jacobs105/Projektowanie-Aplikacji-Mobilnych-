e8a11f5f4667e475ee9fe651308498ed649f28cc
Jacobs105 <32591673+Jacobs105@users.noreply.github.com>
2018-03-09 16:19:31 +0100

    kalkulator: Layout i tryby Simple oraz Advanced
    Menu oraz dwa tryby pracy
    Niedokończony Main.
    TODO----
    Layout Adv .
    onClick przycisków

--- KalkulatorV2/app/src/main/java/com/example/jakub/kalkulatorv2/MainActivity.java
+++ KalkulatorV2/app/src/main/java/com/example/jakub/kalkulatorv2/MainActivity.java
@@ -2,12 +2,65 @@ package com.example.jakub.kalkulatorv2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+   private Button simpleButton,
+    advancedButton,
+    quitButton,
+    button0,
+    button1,
+    button2,
+    button3,
+    button4,
+    button5,
+    button6,
+    button7,
+    button8,
+    button9,
+    buttonADD,
+    buttonSUB,
+    buttonMUL,
+    buttonDIV,
+    buttonEQU,
+    buttonC;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        simpleButton = findViewById(R.id.simpleCalculator);
+        advancedButton = findViewById(R.id.advancedCalculator);
+        quitButton = findViewById(R.id.quitCalculator);
+
+
+
+
+        simpleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               setContentView(R.layout.simple_calculator);
+            }
+        });
+
+        advancedButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setContentView(R.layout.advanced_calculator);
+            }
+        });
+
+        quitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onStop();
+            }
+        });
     }
+
+
+
 }
